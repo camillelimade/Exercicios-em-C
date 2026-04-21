8a1bb6c46083ea4bee38da4df4587941bcb4b6d7
camille <138686617+camillelimade@users.noreply.github.com>
2026-04-21 15:07:01 -0300

    feat: adiciona solução do problema 6 com lógica básica de soma e remoção

--- Atv3Agora/problemaSeis.c
+++ Atv3Agora/problemaSeis.c
@@ -1,4 +1,29 @@
 #include <stdio.h>
 int main () {
-    
+    int qtd, num;
+    int soma = 0;
+    int lista[200];
+    int pos = 0;
+
+    printf("Digite a quantidade de numeros: ");
+    scanf("%d", &qtd);
+
+    for (int i = 0; i < qtd; i++) {
+        printf("Digite um numero: ");
+        scanf("%d", &num);
+
+        if (num == 0) {
+            if (pos > 0) {
+                pos -= 1;
+                soma -= lista[pos];
+            }else {
+                printf("Nada pra remover\n");
+            }
+        } else {
+            lista[pos] = num;
+            soma += num;
+            pos += 1;
+        }
+    }
+    printf("Soma total: %d\n", soma);
 }
